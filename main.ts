7a308eefc899853b174538fe1864531c743b7e08
tommyzhang12345678 <102849446+tommyzhang12345678@users.noreply.github.com>
2022-07-07 10:32:53 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,4 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    voilume += 50
+})
+let voilume = 100
 basic.forever(function () {
+    music.setVolume(voilume)
     if (input.acceleration(Dimension.Y) <= 800) {
         basic.showLeds(`
             # . . . #
@@ -10,5 +15,8 @@ basic.forever(function () {
         music.playTone(262, music.beat(BeatFraction.Quarter))
     } else if (input.acceleration(Dimension.Y) > 800) {
         basic.showIcon(IconNames.Yes)
+    }
+    if (voilume <= 300) {
+        voilume = 50
     }
 })
